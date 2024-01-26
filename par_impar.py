3d4e3c15f8ff5e0b2cff8e2a61bf4fb11fb50056
Luana de Jesus <139658239+luana1003@users.noreply.github.com>
2024-01-26 11:55:56 -0300

    Delete par_impar.py

--- par_impar.py
+++
@@ -1,7 +0,0 @@
-#Programa para verificar se um número é par ou ímpar
-numero = int(input("Digite um número e veja se ele é par ou ímpar: "))
-
-if numero % 2 == 0:
-    print(f"O número {numero} é par")
-else:
-    print(f"O número {numero} é ímpar")
