1d415c4e1322fd5a6ce31cc3fbd6f6a016296ae9
Luana de Jesus <139658239+luana1003@users.noreply.github.com>
2024-01-26 11:56:53 -0300

    Add files via upload

---
+++ Laços de repetição e listas/listando_intervalo_anos.py
@@ -0,0 +1,3 @@
+#Programa para listar anos de 2007 a 2024
+for ano in range (2007,2025):
+    print(ano)
---
+++ Laços de repetição e listas/listando_nomes.py
@@ -0,0 +1,9 @@
+#Programa para listar nomes de A a Z
+def listar_nomes():
+    print("Listando nome de A a Z")
+    nomes = ["Ana", "Bernardo", "Carlos", "Débora", "Eliane", "Fernando", "Gabriel", "Henrique", "Ivone", "Jonas", "Kaique", "Luana", "Miguel", "Nilce", "Osvaldo", "Paulo", "Queila", "Raul", "Sara", "Thiago", "Úrsula", "Valter", "Wanessa", "Yuri", "Zélia"]
+
+    for nome in nomes:
+        print(nome)
+
+listar_nomes()
