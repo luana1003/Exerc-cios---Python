92134e7e37d626259d46094369e9fad5ddd8a193
Luana de Jesus <139658239+luana1003@users.noreply.github.com>
2024-01-26 12:18:48 -0300

    Update listando_intervalo_anos.py

--- Laços de repetição e listas/listando_intervalo_anos.py
+++ Laços de repetição e listas/listando_intervalo_anos.py
@@ -1,3 +1,6 @@
 #Programa para listar anos de 2007 a 2024
 for ano in range (2007,2025):
     print(ano)
+
+#Anotações
+#range(): indica um intervalo número, sendo o primeiro número o de início desse recorte e o último algarismo consistindo no valor final desejado mais 1.
